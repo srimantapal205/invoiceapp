1f8031e57aca1f17590ae00e25d145b808734882
Srimanta Pal <srimantapal205@hotmail.com>
2022-09-14 23:42:35 +0530

    Add navigation bar and responsive design

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,47 @@
 <template>
-  <router-view/>
+  <div>
+    <div v-if="!mobile" class="app flex flex-column">
+      <Navigation />
+      <div class="appContaint flex flex-column">
+        <router-view />
+      </div>
+    </div>
+    <div v-else class="mobileText flex flex-column">
+        <h2>Sorry this app is not supported in Mobile Devices</h2>
+        <p>To use this app please use a Computer and Tablet</p>
+    </div>
+  </div>
 </template>
+
+<script>
+import Navigation from "@/components/Navigation.vue";
+
+export default {
+  name: "app-root",
+  data() {
+    return {
+      mobile: null
+    }
+  },
+  components: {
+    Navigation,
+  },
+  created () {
+    this.checkScreen();
+    window.addEventListener("resize", this.checkScreen)
+  },
+  methods: {
+    checkScreen() {
+      const windowWidth = window.innerWidth
+      if (windowWidth <=750 ) {
+         this.mobile =true;
+         return
+      }
+      this.mobile=false;
+    }
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
@@ -10,9 +51,30 @@
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
-  background-color: #141625;
 }
-
+.app {
+  background-color: #141625;
+  min-height: 100vh;
+  @media (min-width: 900px) {
+    flex-direction: row !important;
+  }
+  .appContaint {
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
+}
+.mobileText{
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color:#141625;
+  color: #ffffff;
+  p{
+    margin-top: 16px;
+  }
+}
 button,
 .button {
   cursor: pointer;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="home container">
+    <!-- Header  -->
+    <div class="header flex">
+    
+    </div>
   </div>
 </template>
 
